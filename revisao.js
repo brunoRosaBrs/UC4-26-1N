e0858f61f4a39959c3e1798b3e00a2da3784a4e1
Bruno <brunorosabrs1234@gmail.com>
2026-07-23 16:50:34 -0300

    Update revisao.js

--- revisao.js
+++ revisao.js
@@ -2,14 +2,16 @@ const ask = require('readline-sync');
 
 /*
 // 1. Peça o nome e idade do usuário e mostre uma mensagem personalizada.
+
 let nome = ask.question('What is your name? ');
 let idade = ask.question('How old are you? ');
 console.log(`The name entered was ${nome}, and the age was ${idade}`);
 */
 
-// 2. Solicite dois números e mostre a soma, subtração, multiplicação e divisão.
 
 /*
+// 2. Solicite dois números e mostre a soma, subtração, multiplicação e divisão.
+
 let number1 = Number(ask.question('Enter the first number to perform the operations: '));
 let number2 = Number(ask.question('Enter the second number to perform the operations: '));
 console.log(`
@@ -21,16 +23,18 @@ console.log(`
 */
 
 
-// 3. Peça uma nota e exiba “Aprovado” ou “Reprovado” usando operador ternário.
 /*
+// 3. Peça uma nota e exiba “Aprovado” ou “Reprovado” usando operador ternário.
+
 let grade = Number(ask.question('Enter the grade you received: '))
 const result = grade >= 7 ? "Approved" : "Failed";
 console.log(result);
 */
 
 
-// 4. Verifique se um número é par ou ímpar usando if/else.
 /*
+// 4. Verifique se um número é par ou ímpar usando if/else.
+
 let number = Number(ask.question(`Enter a number to check if it is odd or even: `));
 if (number % 2 === 0) {
     console.log(`This number is even`);
@@ -40,8 +44,9 @@ if (number % 2 === 0) {
 */
 
 
-// 5. Peça o dia da semana (1-7) e use switch para mostrar o nome do dia.
 /*
+// 5. Peça o dia da semana (1-7) e use switch para mostrar o nome do dia.
+
 let entededDay = Number(ask.question(`Enter a day of the week from 1 to 7: `));
 switch (entededDay) {
     case 1:
@@ -71,8 +76,9 @@ switch (entededDay) {
 */
 
 
-// 6. Crie um mini-cardápio e use switch para mostrar o preço do item escolhido.
 /*
+// 6. Crie um mini-cardápio e use switch para mostrar o preço do item escolhido.
+
 let menu = Number(ask.question(`
 --------MENU---------
 1 - hotdog
@@ -99,8 +105,10 @@ switch (menu) {
 };
 */
 
-// 7. Crie um array de 5 números e calcule a soma usando for.
+
 /*
+// 7. Crie um array de 5 números e calcule a soma usando for.
+
 let numbers = [1, 3, 4, 7, 8];
 let sum = 0
 for (let i = 0; i < 5; i++) {
@@ -110,8 +118,9 @@ console.log(sum);
 */
 
 
-// 8. Faça um loop while que exiba números de 1 a 10.
 /*
+// 8. Faça um loop while que exiba números de 1 a 10.
+
 let number = 1;
 while (number <= 10) {
   console.log(number);
@@ -119,11 +128,114 @@ while (number <= 10) {
 }
 */
 
-// 9. Use for...of para imprimir todos os elementos de um array de frutas.
+
 /*
+// 9. Use for...of para imprimir todos os elementos de um array de frutas.
+
 const fruits = ["apple", "banana", "orange", "pineapple"];
 for (const fruit of fruits) console.log(fruit);
 */
 
-// 10. Use um laço for tradicional para percorrer um array de frutas e imprimir cada elemento no console.
+
+/*
+//10. Use um laço for tradicional para percorrer um array de frutas e imprimir cada elemento no console.
+
+const fruits = ["apple", "banana", "orange", "pineapple"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+*/
+
+
+/*
+// 11. Crie um objeto pessoa com nome e idade e use for...in para imprimir cada propriedade.
+
+const pessoa = {
+    nome: "Bruno",
+    idade: 29 };
+for (const chave in pessoa) {
+  console.log(`${chave}: ${pessoa[chave]}`);
+}
+*/
+
+
+/*
+// 12. Faça um loop que exiba todos os números pares de 0 a 20.
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+*/
+
+
+/*
+// 13. Crie uma função tradicional que receba 2 números e retorne o maior.
+
+function largerNumber(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    else {
+        return num2;
+    }
+    
+}
+
+let num1 = Number(ask.question(`Enter the first number: `));
+let num2 = Number(ask.question(`Enter the second number: `));
+
+const result = largerNumber(num1, num2);
+console.log(`The largest number is: ${result}`);
+*/
+
+
+/*
+// 14. Crie uma função que receba um array de números e retorne a soma de todos os numeros primos.
+
+function sumPrimes(numbers) {
+  let sum = 0;
+
+  for (let number of numbers) {
+    let isPrime = true;
+
+    if (number < 2) {
+      isPrime = false;
+    }
+
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      sum += number;
+    }
+  }
+
+  return sum;
+}
+
+let numbers = [2, 3, 4, 5, 6, 7, 8, 11];
+
+console.log(sumPrimes(numbers));
+*/
+
+
+/*
+// 15. Crie uma função que peça ao usuário seu nome e idade (usando readline-sync) e exiba uma saudação personalizada.
+
+function greeting() {
+  let name = ask.question("Enter your name: ");
+  let age = Number(ask.question("Enter your age: "));
+  console.log("Hello, " + name + "! You are " + age + " years old.");
+}
+
+greeting();
+*/
+
+
+
 
