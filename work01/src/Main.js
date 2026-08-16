cf06fdff15c00966463c42b810d665dfe1c52270
Bruno <brunorosabrs1234@gmail.com>
2026-08-16 01:00:31 -0300

    Delete work01/src directory

--- work01/src/Main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Character_1 = require("./Character");
-const char1 = new Character_1.Character("Vellyan", 200, 4000);
-char1.showInfo();
-console.log(`Damage taken: ${char1.takeDamage(50)}`);
-char1.takeDamage(40);
-char1.showInfo();
--- work01/src/Main.ts
+++
@@ -1,7 +0,0 @@
-import { Character } from "./Character";
-
-const char1 = new Character("Vellyan", 200, 4000);
-char1.showInfo();
-console.log(`Damage taken: ${char1.takeDamage(50)}`);
-char1.takeDamage(40);
-char1.showInfo();
