acd05949c3d8e446ce587090f2dd46457e8cd837
Bruno <brunorosabrs1234@gmail.com>
2026-08-16 01:01:54 -0300

    Add files via upload

---
+++ work01/src/Weapon.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Weapon = void 0;
+class Weapon {
+    constructor(name, damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+    //Getter
+    getName() {
+        return this.name;
+    }
+    getDamage() {
+        return this.damage;
+    }
+    //Setter
+    setName(name) {
+        this.name = name;
+    }
+    setDamage(damage) {
+        if (damage > 0) {
+            this.damage = damage;
+        }
+        else {
+            this.damage = 1;
+        }
+    }
+    //Method
+    showInfo() {
+        console.log(`
+========================
+Weapon
+========================
+Name: ${this.name}
+Damage: ${this.damage}
+            `);
+    }
+}
+exports.Weapon = Weapon;
